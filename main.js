2a8dbf61b1a51d9f29ddc42bb04f053d93fdcae1
Tim <ccu.schaller@outlook.com>
2022-12-10 20:30:48 +0100

    fixed timer

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ class FroniusWattpilot extends utils.Adapter {
 			await createObjectAsync("set_mode", "string", "level", true, true);
 			this.subscribeStates("set_mode");
 
-			await createObjectAsync("set_state", "string", "level", true, true);
+			await createObjectAsync("set_state", "string", "string", true, true);
 			this.subscribeStates("set_state");
 
 			createWsConnection();
